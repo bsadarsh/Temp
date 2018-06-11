07aa12379c4f7e679135b2d5e6a3ccb4f79a7548
bsadarsh <bs.adarsh@gmail.com>
2018-06-11 22:48:59 +0100

    Add files via upload

---
+++ Release/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReleaseToday
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            int i = 20;
+            Console.WriteLine(i);
+            Console.Read();
+        }
+    }
+}
