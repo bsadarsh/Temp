1846bec462e2a9aa3399acfc607611820404cdb0
Adarsh <bs.adarsh@gmail.com>
2018-06-11 23:54:54 +0100

    FirstProjectCommit
    with folder

---
+++ Release_12-06-2018/FirstFolderC/FirstFolderC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstFolderC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
